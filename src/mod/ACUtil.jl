c3bb58e393f1979c9953c8d7436bad09cc0e3bc4
Shozen Dan <xiaoran.duan@gmail.com>
2020-02-29 11:22:13 -0800

    added the signal to noise ratio function

--- src/mod/ACUtil.jl
+++ src/mod/ACUtil.jl
@@ -3,8 +3,11 @@ export
     # modify ac2d output
     ac2d_flatten,
     # modify image
-    make_noisy
+    make_noisy,
+    # signal to noise ratio
+    snr
 using Random
+using LinearAlgebra
 
 """
     ac2d_flatten(x)
@@ -31,6 +34,22 @@ Adds gaussian white noise to an image.
 function make_noisy(x, rng, a::Real)
     noisy = x + a * randn(rng, Float64, size(x))
     return noisy
+end
+
+"""
+    snr(f::AbstractArray{<:Number}, g::AbstractArray{<:Number})
+
+Computes the signal to noise ratio between two signals.
+### Arguments
+`f`: Original signal
+`g`: Noisy signal
+### Returns
+signal to noise ratio(dB)
+"""
+function snr(f::AbstractArray{<:Number}, g::AbstractArray{<:Number})
+    L2_f = norm(f)
+    L2_fg = norm(f-g)
+    return 20*log10(L2_f/L2_fg)
 end
 
 end # module
