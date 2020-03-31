51dcf47f76b229ae55dbd8fd007621604af6d93c
Shozen Dan <xiaoran.duan@gmail.com>
2020-03-31 12:19:59 +0900

    updated iac2d to be compatible for the new ac2d formate

--- src/mod/AC2D.jl
+++ src/mod/AC2D.jl
@@ -25,7 +25,7 @@ function ac1d_col(x, L, P, Q)
     D = J - L + 1
     accoef_matrix_3d = Array{Float64, 3}(undef, num_col, D, num_col)
     for i in 1:num_col
-        accoef_matrix_3d[:,:,i] = fwt_ac(img[:,i],L,P,Q)
+        accoef_matrix_3d[:,:,i] = fwt_ac(x[:,i],L,P,Q)
     end
     return accoef_matrix_3d
 end
@@ -55,52 +55,33 @@ function ac2d(x, L, P, Q)
     for i in 1:D
         accoef_matrix_4d[i,:,:,:] = ac1d_col(accoef_matrix_3d[:,i,:],L,P,Q)
     end
-    accoef_matrix_4d = permutedims(accoef_matrix_4d, [2,4,1,3])
+    accoef_matrix_4d = permutedims(accoef_matrix_4d, [4,2,1,3])
     return accoef_matrix_4d
 end
 
 # ------------ Inverse functions -------------
-function rowinv(x)
 """
-    rowinv(x)
-Performs iwt_ac on rows of each element of the input
-vector.
-
-# Arguments
-- `x`: one element of the ac2d function output vector
-"""
-    n_scale=length(x)
-    num_row,num_col=size(x[1])
-    a = [Array{Float64}(undef,num_row,n_scale) for i=1:num_col]
-    for i=1:num_row
-        for j=1:n_scale
-            a[i][:,j]=x[j][i,:]
-        end
-    end
-    b=[iwt_ac(a[i]) for i=1:length(a)]
-    return transpose(reduce(hcat,b))
-end
-
-function iac2d(ac)
-"""
-    iac2d(ac)
+    iac2d(x)
 
 Performs the inverse 2D autocorrelation wavelet transform.
 
 # Arguments
-- `ac`: ac2d function output
+- `x`: ac2d function output
 """
-    tmp = [rowinv(ac[i]) for i=1:length(ac)]
-    n_scale=length(tmp)
-    num_row,num_col=size(tmp[1])
-    a = [Array{Float64}(undef,num_row,n_scale) for i=1:num_col]
-    for i=1:num_row
-        for j=1:n_scale
-            a[i][:,j]=tmp[j][:,i]
+function iac2d(x)
+    num_row, num_col, D_row, D_col = size(x)
+    accoef_matrix_4d = permutedims(x, [3,2,4,1])
+    accoef_matrix_3d = Array{Float64, 3}(undef, num_col, D_col, num_row)
+    for i in 1:D_row
+        for j in 1:num_row
+            accoef_matrix_3d[:,i,j] = iwt_ac(accoef_matrix_4d[i,:,:,j])
         end
     end
-    b=[iwt_ac(a[i]) for i=1:length(a)]
-    return reduce(hcat,b)
+    reconst = Array{Float64, 2}(undef, 512, 512)
+    for i in 1:num_col
+        reconst[:,i] = iwt_ac(accoef_matrix_3d[:,:,i])
+    end
+    return reconst
 end
 
 end # module
