67bb76dd49537412ad2fb7be6aa647506d0fa67b
Shozen Dan <xiaoran.duan@gmail.com>
2020-04-02 18:19:38 +0900

    fixed bug in ac2d and iac2d functions

--- src/mod/AC2D.jl
+++ src/mod/AC2D.jl
@@ -23,9 +23,33 @@ function ac1d_col(x, L, P, Q)
     num_row, num_col = size(x)
     J = trunc(Integer, log2(num_col))
     D = J - L + 1
-    accoef_matrix_3d = Array{Float64, 3}(undef, num_col, D, num_col)
+    accoef_matrix_3d = Array{Float64, 3}(undef, num_row, D, num_col)
     for i in 1:num_col
         accoef_matrix_3d[:,:,i] = fwt_ac(x[:,i],L,P,Q)
+    end
+    return accoef_matrix_3d
+end
+
+"""
+  ac1d_row(x, L, P, Q)
+
+Computes the row wise 1D fwt_ac coeficients for 2D signals.
+
+### Arguments
+- `x`: 2D signal (n ,m) matrix
+- `L`: Decomposition level
+- `P`: Low AC shell filter
+- `Q`: High AC shell filter
+
+Returns a multidimensional array of (num_col, levels_of_decomposition)
+"""
+function ac1d_row(x, L, P, Q)
+    num_row, num_col = size(x)
+    J = trunc(Integer, log2(num_row))
+    D = J - L + 1
+    accoef_matrix_3d = Array{Float64, 3}(undef, num_col, D, num_row)
+    for i in 1:num_row
+        accoef_matrix_3d[:,:,i] = fwt_ac(x[i,:],L,P,Q)
     end
     return accoef_matrix_3d
 end
@@ -53,9 +77,9 @@ function ac2d(x, L, P, Q)
     accoef_matrix_3d = ac1d_col(x, L, P, Q)
     accoef_matrix_4d = Array{Float64, 4}(undef, D, num_row, D, num_col)
     for i in 1:D
-        accoef_matrix_4d[i,:,:,:] = ac1d_col(accoef_matrix_3d[:,i,:],L,P,Q)
+        accoef_matrix_4d[i,:,:,:] = ac1d_row(accoef_matrix_3d[:,i,:],L,P,Q)
     end
-    accoef_matrix_4d = permutedims(accoef_matrix_4d, [4,2,1,3])
+    accoef_matrix_4d = permutedims(accoef_matrix_4d, [4,2,3,1])
     return accoef_matrix_4d
 end
 
@@ -70,14 +94,14 @@ Performs the inverse 2D autocorrelation wavelet transform.
 """
 function iac2d(x)
     num_row, num_col, D_row, D_col = size(x)
-    accoef_matrix_4d = permutedims(x, [3,2,4,1])
-    accoef_matrix_3d = Array{Float64, 3}(undef, num_col, D_col, num_row)
-    for i in 1:D_row
-        for j in 1:num_row
-            accoef_matrix_3d[:,i,j] = iwt_ac(accoef_matrix_4d[i,:,:,j])
+    accoef_matrix_4d = permutedims(x, [4,2,3,1])
+    accoef_matrix_3d = Array{Float64, 3}(undef, num_row, D_col, num_col)
+    for i in 1:D_col
+        for j in 1:num_col
+            accoef_matrix_3d[j,i,:] = iwt_ac(accoef_matrix_4d[i,:,:,j])
         end
     end
-    reconst = Array{Float64, 2}(undef, 512, 512)
+    reconst = Array{Float64, 2}(undef, num_row, num_col)
     for i in 1:num_col
         reconst[:,i] = iwt_ac(accoef_matrix_3d[:,:,i])
     end
