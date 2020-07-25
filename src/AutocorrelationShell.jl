6e0dad3a28e91c145712f85965a03cdaa2471672
Shozen Dan <xiaoran.duan@gmail.com>
2020-07-26 00:58:21 +0900

    Implemented autocorrelation wavelet packets

--- src/AutocorrelationShell.jl
+++ src/AutocorrelationShell.jl
@@ -7,8 +7,9 @@ include("mod/AC1D.jl")
 include("mod/AC2D.jl")
 include("mod/ACThreshold.jl")
 include("mod/ACUtil.jl")
+include("mod/ACWPT1D.jl")
 
 using Reexport
-@reexport using .AC1D, .AC2D, .ACThreshold, .ACUtil
+@reexport using .AC1D, .AC2D, .ACThreshold, .ACUtil, .ACWPT1D
 
 end # module
