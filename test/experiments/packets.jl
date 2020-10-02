142240c82604fa50ea87ac747a746a4af17042f8
Shozen Dan <xiaoran.duan@gmail.com>
2020-10-02 11:47:49 +0900

    adding tests

--- test/experiments/packets.jl
+++ test/experiments/packets.jl
@@ -1,58 +1,6 @@
-using Wavelets, AutocorrelationShell, Plots
-
-function echant(n::Integer, d::Integer, b::Integer)
-    return (b + 1):(2^d):n
-end
+using Wavelets, AutocorrelationShell, Plots, LinearAlgebra, AbstractTrees
 
 ## base node methods
-function leftchild(data, parent::BinaryNode)
-    !isdefined(parent, :left) || error("left child is already assigned")
-    node = typeof(parent)(data, parent)
-    parent.left = node
-end
-
-function rightchild(data, parent::BinaryNode)
-    !isdefined(parent, :right) || error("right child is already assigned")
-    node = typeof(parent)(data, parent)
-    parent.right = node
-end
-
-## Wavelet Packets functions
-function binary_ac1d_new(x::Vector{T}, node::BinaryNode,
-    P::Vector{T}, Q::Vector{T}, depth::Integer) where T<:Real
-
-    n = length(x)
-    max_depth = dyadlength(x) # maximum tree depth
-    left = zeros(n)
-    right = zeros(n)
-
-    if depth <= max_depth
-        for b = 0:(2^(depth-2) - 1) # depth starts from 2
-            s = x[echant(n, depth-2, b)]
-            h = ac_filter(s, Q)
-            l = ac_filter(s, P)
-            left[echant(n, depth-2, b)] = l
-            right[echant(n, depth-2, b)] = h
-        end
-
-        # left
-        leftchild(left, node)
-        binary_ac1d_new(left, node.left, P, Q, depth+1)
-
-        # right
-        rightchild(right, node)
-        binary_ac1d_new(right, node.right, P, Q, depth+1)
-    end
-end
-
-function acwpt_new(x::Vector{T}, P::Vector{T}, Q::Vector{T}) where T<:Real
-
-    root = BinaryNode(x) # original signal
-    binary_ac1d_new(x, root, P, Q, 2)
-
-    return root
-end
-
 Q = qfilter(wavelet(WT.db2));
 P = pfilter(wavelet(WT.db2));
 
@@ -65,13 +13,15 @@ wentropy(X, NormEntropy())
 
 tree = acwpt_new(X, P, Q);
 
-# print_tree(tree) Too large to print
+decomp = fwt_ac_new(X, 0, P, Q);
 
+norm(collect(PostOrderDFS(tree))[1].data - decomp[:,1]) < 1e-15
+
+# print_tree(tree) Too large to print
 l = plot(1:256, tree.left.data, legend=false, ylims=(-0.3, 0.4))
 wentropy(tree.left.data, NormEntropy())
 png("/Users/shozendan/Desktop/left")
 
-#tree.right.data
 r = plot(1:256, tree.right.data, legend=false)
 r = plot(1:256, tree.right.data, legend=false, ylims=(-0.3, 0.4))
 wentropy(tree.right.data, NormEntropy())
@@ -94,5 +44,3 @@ png("/Users/shozendan/Desktop/left-left")
 lr = plot(1:256, tree.left.right.data, legend=false, ylims=(-0.3, 0.4))
 wentropy(tree.left.right.data, NormEntropy())
 png("/Users/shozendan/Desktop/left-right")
-
-acwptPostOrderBestBasis(tree)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,26 @@
-using Test, AutocorrelationShell, Wavelets, LinearAlgebra
+using Test, AutocorrelationShell, Wavelets, LinearAlgebra, AbstractTrees
 
+## Autocorrelation Wavelet Transform 
 @test begin
     Q = qfilter(wavelet(WT.db2));
     P = pfilter(wavelet(WT.db2));
 
     x = zeros(256); x[128] = 1;
-
     decomp = acwt(x, L=2, P=P, Q=Q)
 
-    norm(x - iacwt(decomp)) < 1e-14
+    norm(x - iacwt(decomp)) < 1e-15
+end
+
+## Autocorrelation Wavelet Packet Transform
+@test begin
+    Q = qfilter(wavelet(WT.db2));
+    P = pfilter(wavelet(WT.db2));
+
+    x = zeros(256); x[128] = 1;
+    X = acwt(x, L=2, P=P, Q=Q)[:,4];
+
+    decomp = acwt(X, L=0, P=P, Q=Q);
+    tree = acwpt(X, P, Q);
+
+    norm(collect(PostOrderDFS(tree))[1].data - decomp[:,1]) < 1e-15
 end
